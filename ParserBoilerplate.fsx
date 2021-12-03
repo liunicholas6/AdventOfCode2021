78cfdb573715ecc98c5dd48dca7cd8f5c1095e54
Nick Liu <86247452+liunicholas6@users.noreply.github.com>
2021-12-02 23:52:33 -0500

    Setup for Day 3

---
+++ ParserBoilerplate.fsx
@@ -0,0 +1,9 @@
+#r "nuget: FParsec"
+
+open FParsec
+type Parser<'t> = Parser<'t, unit>
+
+let parse p str =
+    match run p str with
+    | Success (result, _, _) -> result
+    | Failure (errorMsg, _, _) -> failwith errorMsg

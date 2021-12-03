8e5aa471d82d38eed1d11746a8f085a66d40893c
Nick Liu <86247452+liunicholas6@users.noreply.github.com>
2021-12-03 02:24:16 -0500

    Formatting + Day 3 cleanup

--- Day2.fsx
+++ Day2.fsx
@@ -1,6 +1,6 @@
 open System
 
-let (|Prefix|_|) (p:string) (s:string) =
+let (|Prefix|_|) (p: string) (s: string) =
     if s.StartsWith(p) then
         Some(s.Substring(p.Length))
     else
@@ -13,41 +13,46 @@ type OpCode =
 
 let parse s =
     match s with
-    | Prefix "forward " num -> Forward (Int64.Parse num)
-    | Prefix "up " num -> Up (Int64.Parse num)
-    | Prefix "down " num -> Down (Int64.Parse num)
+    | Prefix "forward " num -> Forward(Int64.Parse num)
+    | Prefix "up " num -> Up(Int64.Parse num)
+    | Prefix "down " num -> Down(Int64.Parse num)
     | _ -> failwith "failed to parse"
 
 let input =
-    IO.File.ReadAllLines "Day2.txt"
-    |> Seq.map parse
+    IO.File.ReadAllLines "Day2.txt" |> Seq.map parse
 
 let depth =
     input
-    |> Seq.fold (fun acc c ->
-        acc +
-        match c with
-        | Up num -> (num * -1L)
-        | Down num -> num
-        | _ -> 0L) 0L
+    |> Seq.fold
+        (fun acc c ->
+            acc
+            + match c with
+              | Up num -> (num * -1L)
+              | Down num -> num
+              | _ -> 0L)
+        0L
 
 let horizontal =
     input
-    |> Seq.fold (fun acc c ->
-        acc +
-        match c with
-        | Forward num -> num
-        | _ -> 0L) 0L
+    |> Seq.fold
+        (fun acc c ->
+            acc
+            + match c with
+              | Forward num -> num
+              | _ -> 0L)
+        0L
 
 let problem1 = depth * horizontal
 
 let depth2 =
     input
-    |> Seq.fold (fun (aim, currDepth) c ->
-        match c with
-        | Up num -> (aim - num, currDepth)
-        | Down num -> (aim + num, currDepth)
-        | Forward num -> (aim, currDepth + aim * num)) (0L, 0L)
-        |> snd
+    |> Seq.fold
+        (fun (aim, currDepth) c ->
+            match c with
+            | Up num -> (aim - num, currDepth)
+            | Down num -> (aim + num, currDepth)
+            | Forward num -> (aim, currDepth + aim * num))
+        (0L, 0L)
+    |> snd
 
 let problem2 = horizontal * depth2
--- Day3.fsx
+++ Day3.fsx
@@ -4,28 +4,31 @@ let parse s = s
 
 let input =
     IO.File.ReadLines "Day3.txt"
-    |> Seq.map (fun s -> s |> seq |> Seq.toArray)
+    |> Seq.map (fun s -> s |> Seq.toArray)
     |> Seq.toArray
 
-let mostCommon (array : char[][]) index =
+let mostCommonAtIndex index (array: char [] []) =
     (array2D array).[*, index]
-        |> Seq.fold (fun acc hd ->
-            acc +
-            match hd with
-            | '0' -> -1
-            | '1' -> 1
-            | _ -> failwith "oh no") 0
-        |> fun i -> if i >= 0 then '1' else '0'
+    |> Seq.fold
+        (fun acc hd ->
+            acc
+            + match hd with
+              | '0' -> -1
+              | '1' -> 1
+              | _ -> failwith "oh no")
+        0
+    |> fun i -> if i >= 0 then '1' else '0'
 
-let mostCommonInInput =
-    {0 .. 11}
-    |> Seq.map (mostCommon input)
+let rate =
+    let mostCommonInInput =
+        input.[0]
+        |> Array.mapi (fun i _ -> mostCommonAtIndex i input)
 
-let rate i =
-    mostCommonInInput
-    |> Seq.map (fun bit -> if bit = i then '0' else '1')
-    |> String.Concat
-    |> fun s -> Convert.ToInt64(s, 2)
+    fun i ->
+        mostCommonInInput
+        |> Seq.map (fun bit -> if bit = i then '0' else '1')
+        |> String.Concat
+        |> fun s -> Convert.ToInt64(s, 2)
 
 let problem1 =
     let gamma = rate '1'
@@ -33,15 +36,16 @@ let problem1 =
     gamma * epsilon
 
 let rate2 f =
-    (input, {0 .. 11})
-    ||> Seq.fold (fun acc hd ->
-        match Array.tryExactlyOne acc with
-        | None -> 
-            Array.filter (fun (arr : char[]) ->
-                f arr.[hd] (mostCommon acc hd)) acc
-        | _ -> acc
-    )
-    |> Array.exactlyOne
+
+    let rec loop (index: int) (remaining: char [] []) =
+        match Array.tryExactlyOne remaining with
+        | Some final -> final
+        | None ->
+            Array.filter (fun (arr: char []) -> f arr.[index] (mostCommonAtIndex index remaining)) remaining
+            |> loop (index + 1)
+
+    input
+    |> loop 0
     |> String.Concat
     |> fun s -> Convert.ToInt64(s, 2)
 

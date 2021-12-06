b1e5108ad9c81d9c7ff6343cca41ef9430f730a7
Nick Liu <86247452+liunicholas6@users.noreply.github.com>
2021-12-06 02:54:21 -0500

    Day6 inplace update

--- Day6.fsx
+++ Day6.fsx
@@ -2,6 +2,7 @@
 
 open FParsec
 open System
+
 type Parser<'t> = Parser<'t, unit>
 
 let parse p str =
@@ -17,33 +18,33 @@ let input =
 
 let nextday l =
     l
-    |> List.collect(fun days ->
+    |> List.collect (fun days ->
         match days with
-        | 0 -> [6; 8]
-        | x -> [x - 1])
+        | 0 -> [ 6; 8 ]
+        | x -> [ x - 1 ])
 
 let rec reapply f acc count =
     match count with
     | 0 -> acc
     | _ -> reapply f (f acc) (count - 1)
 
-let problem1 =
-    reapply nextday input 80
-    |> List.length
+let problem1 = reapply nextday input 80 |> List.length
 
-let nextday2 (arr : Int64 array) =
-    let newarr = Array.zeroCreate 9
+let nextday2 (arr: Int64 array) =
     let zeroes = arr.[0]
-    for i in List.rev [1..8] do
-        newarr.[i - 1] <- arr.[i]
-    newarr.[6] <- newarr.[6] + zeroes
-    newarr.[8] <- zeroes
-    newarr
+
+    for i in [ 1 .. 8 ] do
+        arr.[i - 1] <- arr.[i]
+
+    arr.[6] <- arr.[6] + zeroes
+    arr.[8] <- zeroes
+    arr
 
 let problem2 =
     let arr = Array.zeroCreate 9
+
     input
     |> List.countBy id
-    |> List.iter (fun (timer, count) -> arr.[timer] <- count |> Convert.ToInt64)
-    reapply nextday2 arr 256
-    |> Array.sum
+    |> List.iter (fun (timer, count) -> arr.[timer] <- Convert.ToInt64 count)
+
+    reapply nextday2 arr 256 |> Array.sum

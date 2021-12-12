6bfc8dde5ee5e45770bb5bd19c45547a18a41a81
CrypticSpren <86247452+CrypticSpren@users.noreply.github.com>
2021-12-11 23:50:22 -0500

    Day11

--- Day11.fsx
+++ Day11.fsx
@@ -13,5 +13,53 @@ let input =
     |> parse
     |> array2D
 
-let step =
-    let rec loop 2DArray
+let removeInvalidIndices arr =
+    List.filter (fun (x, y) -> x >= 0 && y >= 0 && x < Array2D.length1 arr && y < Array2D.length2 arr)
+
+let getMooreHood (xPos, yPos) arr =
+    List.allPairs [-1; 0; 1] [-1; 0; 1]
+    |> List.filter(fun p -> p <> (0, 0))
+    |> List.map (fun (x, y) -> xPos + x, yPos + y)
+    |> removeInvalidIndices arr
+
+let step old_octopi=
+
+    let octopi =
+        old_octopi
+        |> Array2D.map(fun i -> i + 1)
+
+    let mutable loopRunning = true
+    let mutable flashes = 0
+
+    while loopRunning do
+        loopRunning <- false
+        octopi
+        |> Array2D.iteri (fun i j v ->
+            if v > 9 then
+                loopRunning <- true
+                flashes <- flashes + 1
+                octopi.[i, j] <- 0
+                getMooreHood (i, j) octopi
+                |> List.iter(fun (x, y) -> 
+                    if octopi.[x, y] <> 0 then
+                        octopi.[x, y] <- octopi.[x, y] + 1))
+    
+    (flashes, octopi)
+
+
+let problem1 =
+    [1 .. 100]
+    |> Seq.fold(fun (flashes, octopi) _ ->
+        let (newFlashes, newOctopi) = step octopi
+        (flashes + newFlashes, newOctopi)) (0, input)
+    |> fst
+
+let problem2 =
+    let rec loop count octopi =
+        let newOctopi = snd (step octopi)
+        let mutable allFlashed = true
+        newOctopi
+        |> Array2D.iter(fun x -> if x <> 0 then allFlashed <- false)
+        if allFlashed then count else loop (count + 1) newOctopi
+    
+    loop 1 input
